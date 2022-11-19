b82b3b161352912a893b48fa4c9aa3c344514161
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-19 21:39:27 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+let messages = [];
+// writeCards(): returns an array of thank you messages for each name provided to the function
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// countDown(): invokes console.log once for each number, counting down from the number provided to zero:
+// logs each number when counting down, starting from the number provided:
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+countDown(4);
